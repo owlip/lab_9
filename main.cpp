9ce1a3cab2fe5cf1a1de3fc35227e11cf6985457
owlip <danilevich2002@gmail.com>
2021-05-18 13:05:18 +0300

    добавил функцию main и ввод 2 чисел

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "std.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    setlocale(LC_ALL, "Rus");
+ 
+    int number1 ;
+    int number2 ;
+    cout << "Введите целое число 1: ";
+    cin >> number1;
+    cout << "Введите целое число 2: ";
+    cin >> number2;
+
+    getchar();
+    return 0;	
+}
